3ed7ff815972bf3e200b4574999d17865bdfdf34
dbtkdal3690 <dbtkdal1590@gmail.com>
2021-04-27 11:50:07 +0900

    ssg

---
+++ src/main/kotlin/day0427.kt
@@ -0,0 +1,66 @@
+import java.lang.NullPointerException
+
+fun readLineTrim() = readLine()!!.trim()
+
+fun main() {
+    println("== SIMPLE SSG 시작 ==")
+
+    while (true) {
+        print("명령어)")
+        val command = readLineTrim()
+
+        val rq = Rq(command)
+        println(rq.getStringParam("title", "1") == "1" ) // true
+        // println(rq.getIntParam("id) == 1) // true
+    }
+
+    println("== SIMPLE SSG 끝 ==")
+}
+
+class Rq(command: String) {
+    val actionPath: String
+    val paramMap: Map<String, String>
+
+    init {
+        val commandBits = command.split("?", limit = 2)
+
+        actionPath = commandBits[0].trim()
+        val queryStr = if (commandBits.lastIndex == 1 && commandBits[1].isNotEmpty()) {
+            commandBits[1].trim()
+        } else {
+            ""
+        }
+
+        paramMap = if (queryStr.isEmpty()) {
+            mapOf()
+        } else {
+            val paramMapTemp = mutableMapOf<String, String>()
+
+            val queryStrBits = queryStr.split("&")
+
+            for (queryStrBit in queryStrBits) {
+                val queryStrBitBits = queryStrBit.split("=", limit = 2)
+                val paramName = queryStrBitBits[0]
+                val paramValue = if (queryStrBitBits.lastIndex == 1 && queryStrBitBits[1].isNotEmpty()) {
+                    queryStrBitBits[1].trim()
+                } else {
+                    ""
+                }
+
+                if (paramValue.isNotEmpty()) {
+                    paramMapTemp[paramName] = paramValue
+                }
+            }
+
+            paramMapTemp.toMap()
+        }
+    }
+
+    fun getStringParam(name: String, default: String): String {
+        return paramMap[name] ?: default
+    }
+
+    fun getIntParam(name: String): Int {
+        return paramMap[name]!!.toInt()
+    }
+}
